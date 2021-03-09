56f454c8de494fd193d823b1dd228be53dba5eb7
意大利麵的最後希望 <s0610078@my.cmsh.cyc.edu.tw>
2021-03-09 16:34:09 +0800

    Create d649: 數字三角形.cpp

---
+++ Basic/d649: 數字三角形/d649: 數字三角形.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    while (cin >> n && n != 0)
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = n - 1; j > i; j--)
+                cout << "_";
+
+            for (int j = 0; j <= i; j++)
+                cout << "+";
+
+            cout << endl;
+            cout << endl;
+        }
+    return 0;
+}
