c4b27b7ebf6966deddcc190c9b95db4f07c344ab
Calvin <s0610078@my.cmsh.cyc.edu.tw>
2021-08-10 00:03:58 +0800

    new

--- Basic/a001_哈囉/a001_哈囉.cpp
+++ Basic/a001_哈囉/a001_哈囉.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 int main()
 {
-	string a;
-	while(cin >> a)
-	cout << "hello, "<< a << '\n';
+    //建立一個input的字串a
+    string a;
+    //當輸入字串a的時候就會輸出"hello, a"
+    while (cin >> a)
+        cout << "hello, " << a << '\n';
+    return 0;
 }
