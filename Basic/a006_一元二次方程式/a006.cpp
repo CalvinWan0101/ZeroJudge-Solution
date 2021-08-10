5e2a81c4139faf3cb13dea6414508e513b614ccf
Calvin <s0610078@my.cmsh.cyc.edu.tw>
2021-08-11 02:17:51 +0800

    a006

--- Basic/a006_一元二次方程式/a006.cpp
+++ Basic/a006_一元二次方程式/a006.cpp
@@ -1,14 +1,20 @@
+// 題目 : https://zerojudge.tw/ShowProblem?problemid=a006
+// 作者 : Calvin Wan
+// 時間 : 2021/08/11
+
+// pow跟sqrt的結果都是倍精度的，如果處理的數字太大可能會有誤差
 #include <iostream>
-#include<cmath>
+#include <cmath>
 using namespace std;
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
 	int a, b, c;
-	int  x1, x2, x3;
+	int x1, x2, x3;
 	cin >> a >> b >> c;
+	// 判別式>0 則有兩相異根
 	if (pow(b, 2) - 4 * a * c > 0)
 	{
-		//Two different roots x1=2 , x2=-5
+		// 帶入公式
 		x1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
 		x2 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
 		if (x1 > x2)
@@ -16,12 +22,14 @@ int main(int argc, char** argv)
 		else if (x1 < x2)
 			cout << "Two different roots x1=" << x2 << " , x2=" << x1 << '\n';
 	}
+	// 判別式=0 則有一根
 	else if ((pow(b, 2) - 4 * a * c) == 0)
 	{
 		x3 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
-		cout << "Two same roots x=" << x3;
+		cout << "Two same roots x=" << x3 << '\n';
 	}
+	// 判別式<0 無實根
 	else
-		cout << "No real root";
+		cout << "No real root" << '\n';
 	return 0;
 }
