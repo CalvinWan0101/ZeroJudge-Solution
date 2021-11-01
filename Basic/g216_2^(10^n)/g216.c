3c6b7781131898ef2a4b8613b870a5121f0325da
CalvinWan0101 <s0610078@my.cmsh.cyc.edu.tw>
2021-11-01 21:21:26 +0800

    g216

---
+++ Basic/g216_2^(10^n)/g216.c
@@ -0,0 +1,13 @@
+// 題目 : https://zerojudge.tw/ShowProblem?problemid=g216
+// 作者 : Calvin Wan
+// 時間 : 2021/11/01
+
+#include <stdio.h>
+
+int main()
+{
+    int User_Input;
+    scanf("%d", &User_Input);
+    printf("2^(10^%d)", User_Input);
+    return 0;
+}
