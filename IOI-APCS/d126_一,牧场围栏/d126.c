b745232cda49af92ca0cf91ca20225101d4ced8f
CalvinWan <s0610078@my.cmsh.cyc.edu.tw>
2022-01-10 02:11:47 +0800

    Create d126.c

---
+++ IOI-APCS/d126_一,牧场围栏/d126.c
@@ -0,0 +1,13 @@
+// 題目 : https://zerojudge.tw/ShowProblem?problemid=d126
+// 作者 : Calvin Wan
+// 時間 : 2022/01/10
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    while (scanf("%d%d", &a, &b) != EOF)
+        printf("%d\n", (a + b) * 2);
+    return 0;
+}
